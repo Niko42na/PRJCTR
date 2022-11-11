ada00c7048b5282ac0763825e08498fc7573740c
Niko42na <mykrom42na@gmail.com>
2022-11-11 20:50:52 +0200

    added examples for webinar7

--- script.js
+++ script.js
@@ -1,77 +1,133 @@
 `use strict`;
 
-// Основи роботи з функціями
+// Продвинута робота з функціями
 
-// function sayHello(name = 'Woman') {
-// 	let bonus = 'mrs';
-// 	console.log(bonus);
-// 	console.log(`Hello ${bonus} ${name}, you get ${100+2} dollars`);
+// Про контекст виконання на приклад рекурсії та ітерації
+
+// ітеративна сума
+
+// function iterativeSum(x) {
+// 	// console.log('iterativeSum x - ', x);
+//   	let result = 0;
+// 	for (let i = 0; i <= x; i++) {
+// 		result += i;
+// 	}
+// 	return result;
 // }
+// console.log(iterativeSum(5));
 
+// рекурсивна сума
 
-// function squared(number) {
-// 	return number * number;
-// };
-
-// let result = squared(2);
-
-// console.log(result);
-
-// let counter = 0;
-
-// function sayGreeting(age) {
-// 	if (isNaN(+age)) {
-// 		return 'Чи можете ввести число?';
-// 	} else if (age < 18) {
-// 		return 'Привіт!';
-// 	} else if (35 > age) {
-// 		return 'Вітаю!';
-// 	} else {
-// 		return 'Доброго дня';
+// function recursiveSum(x) {
+// 	console.log('recursiveSum x --> ', x);
+// 	if (x === 1) {
+// 		return x;
 // 	}
-// };
-
-// const say = sayGreeting(25);
-
-// const arr = [1, 2, 3];
-
-// const updatedArr = arr.map(num => squared(num));
-
-// let squared = function(number) {
-// 	return number * number;
-// };
-
-// console.log(updatedArr);
-
-// let age = prompt('Скільки вам років?', 10);
-
-// let welcome;
-
-// if (age < 18) {
-// 	welcome = function() {
-// 		console.log('Привіт!');
-// 	}
-// } else if (35 > age) {
-// 	welcome = function() {
-// 		console.log('Доброго дня!');
-// 	}
+// 	return x+recursiveSum(x-1);
 // }
+// console.log(recursiveSum(5));
 
-// welcome();
+// залишкові параметри та оператор розширення
 
-// let functionSometheing = (a, b) => {
-// 	let c = a*b*b;
-// 	return c;
-// }; 
+	// function simpleSum(a, b, ...args) {
+	//   console.log('arguments - ', arguments);
+	//   let sum = a + b;
+	//   for (let arg of args) {
+	//   	sum += arg;
+	//   }
+	//   return sum;
+	// }
+	// console.log(simpleSum(1,2,3,4,12,23));
 
-// console.log(sum(4,5));
+	// let arr1 = [1, -2, 3, 4];
+	// let arr2 = [8, 3, -8, 12];
+	// console.log( Math.max(1, ...arr1, 2, ...arr2) ); 
+
+	// let number = `456`;
+	// console.log([...number] );
+
+// замикання
+
+	// let price1 = 1;
+
+	// function simpleSum(a, b) {
+	// 	let innerPrice = 2;
+	//   	return a + b + innerPrice;
+	// }
+	// price1 = 3;
+	// let innerPrice = 20;
+	// console.log(simpleSum(price1));
 
 
-function recSumTo(number) {
-	if(number === 1) {
-		return 1;
-	}
-	return number + recSumTo(number-1);
-}
+	// function makeWorker() {
+	//   let name = "Петро";
+	//   return function() {
+	//     console.log(name);
+	//   };
+	// }
+	// let name = "Іван";
+	// // create a function
+	// let work = makeWorker();
+	// // call it
+	// work();
 
-console.log(recSumTo(10));
+// створення декількох каунтерів
+
+	// function makeCounter() {
+	// 	let count = 0;
+
+	// 	return function() {
+	// 		return ++count;
+	// 	};
+	// }
+
+	// let counter = makeCounter();
+
+	// console.log( counter() );  // 1
+	// console.log( counter() );  // 2
+	// console.log( counter() );  // 3
+
+	// let counter2 = makeCounter();
+
+	// console.log( counter2() );  
+	// console.log( counter2() );  
+	// console.log( counter2() ); 
+
+//IIFE
+	// (function() {
+	// 	console.log('Hi')
+	// })();
+
+// глобальний об'єкт
+
+	// globalThis.nextTargets = {
+	// 	city: 'Moskow'
+	// }
+
+	// console.log(globalThis.nextTargets.city);
+
+// об'єкт функції
+
+	// function recursiveSum(x, y, ...z) {
+	// 	console.log('recursiveSum x --> ', x);
+	// 	if (x === 1) {
+	// 		return x;
+	// 	}
+	// 	return x+recursiveSum(x-1);
+	// }
+
+	// console.log(recursiveSum.length);
+
+
+	// function sayHi() {
+	// 	// console.log('callingCounter', callingCounter);
+	// 	sayHi.callingCounter++;
+	// }
+
+	// sayHi.callingCounter = 0;
+
+	// sayHi();
+	// sayHi();
+	// sayHi();
+
+	// console.log('sayHi.callingCounter -- ', sayHi.callingCounter);
