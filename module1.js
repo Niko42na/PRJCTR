cfe00915bf378a7a6b0aba7b6f6cb94ab8e4e140
Niko42na <mykrom42na@gmail.com>
2023-02-03 20:28:20 +0200

    added js examples

---
+++ module1.js
@@ -0,0 +1,16 @@
+function sum (a, b) {
+  return hiddenFunctionSum(a, b);
+}
+
+
+function hiddenFunctionSum(a, b) {
+  return a + b;
+}
+
+const user = {
+  name: 'Yurko',
+}
+
+console.log('hello!');
+
+export { sum, user }; 
